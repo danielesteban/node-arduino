a1b9a6d71884ffc6ca20e7592b4584c967630c0e
Daniel Esteban Nombela <dani@hellacoders.com>
2013-03-22 02:27:34 +0100

    initial commit

---
+++ arduino.js
@@ -0,0 +1,49 @@
+var SerialPort = require('serialport'),
+	EventEmitter = require('events').EventEmitter,
+	serial, rxBuf, rxBufLen;
+
+module.exports = new EventEmitter();
+
+function processData() {
+	if(rxBufLen === 0) {
+		rxBufLen = rxBuf[0];
+		rxBuf = rxBuf.slice(1);
+	}
+	
+	if(rxBuf.length >= rxBufLen) {
+		module.exports.emit('req', rxBuf[0], rxBuf[1], rxBuf.slice(2, rxBufLen));
+		rxBuf = rxBuf.slice(rxBufLen);
+		rxBufLen = 0;
+		rxBuf.length && processData();
+	}
+}
+
+module.exports.connect = function(port) {
+	serial = new SerialPort.SerialPort(port || '/dev/tty.usbserial', {
+		parser: SerialPort.parsers.raw,
+		baudrate: 19200
+	});
+	rxBufLen = 0;
+	serial.on('data', function (data) {
+		if(!rxBuf) rxBuf = data;
+		else rxBuf = Buffer.concat([rxBuf, data]);
+		processData();
+	});
+};
+
+module.exports.req = function(device, func, data) {
+	if(!serial) throw new Error('You should call Arduino.connect() before calling Arduino.req()');
+	var buffer = Buffer.concat([new Buffer([device, func]), data]);
+	serial.write(new Buffer([buffer.length]));
+	serial.write(buffer);
+};
+
+module.exports.numToBuffer = function(num, length) {
+	length = length || 4;
+	var buff = new Buffer(length);
+	for(var x=length - 1;x>=0;x--) {
+	   buff[x] = num & (255);
+	   num = num / 256;
+	}
+	return buff;
+}
